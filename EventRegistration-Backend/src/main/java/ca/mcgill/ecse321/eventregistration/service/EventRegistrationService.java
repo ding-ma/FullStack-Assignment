7b686a7aac4bcbb919fcbcf5d1ed7411bb0d1bb4
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-31 14:07:06 -0400

    Circus tests are passing

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/service/EventRegistrationService.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/service/EventRegistrationService.java
@@ -199,8 +199,39 @@ public class EventRegistrationService {
 	}
 	
 	@Transactional
-	public Circus createCircus(String name, Date circusDate, Time valueOf, Time valueOf1, String company) {
+	public Circus createCircus(String name, Date circusDate, Time startTime, Time endTime, String company) {
+		String error = "";
+		if (name == null || name.trim().length() == 0) {
+			error = error + "Event name cannot be empty! ";
+		} else if (eventRepository.existsById(name)) {
+			throw new IllegalArgumentException("Event has already been created!");
+		}
+		if (circusDate == null) {
+			error = error + "Event date cannot be empty! ";
+		}
+		if (startTime == null) {
+			error = error + "Event start time cannot be empty! ";
+		}
+		if (endTime == null) {
+			error = error + "Event end time cannot be empty! ";
+		}
+		if (endTime != null && startTime != null && endTime.before(startTime)) {
+			error = error + "Event end time cannot be before event start time!";
+		}
+		if (company == null || company.trim().length() == 0) {
+			error = error + "Circus company cannot be empty!";
+		}
+		error = error.trim();
+		if (error.length() > 0) {
+			throw new IllegalArgumentException(error);
+		}
 		Circus circus = new Circus();
+		circus.setName(name);
+		circus.setDate(circusDate);
+		circus.setStartTime(startTime);
+		circus.setEndTime(endTime);
+		circus.setCompany(company);
+		circusRepository.save(circus);
 		return circus;
 	}
 	
