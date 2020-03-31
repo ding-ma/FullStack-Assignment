fadfa139da2a943598fb888f27642c77b147d49a
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-31 13:45:14 -0400

    had a bug in Circus.java. Now application can run

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Circus.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Circus.java
@@ -1,21 +1,11 @@
 package ca.mcgill.ecse321.eventregistration.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
+
 
 @Entity
 public class Circus extends Event{
     private String company;
-    private int id;
-    
-    @Id
-    public int getId() {
-        return id;
-    }
-    
-    public void setId(int id) {
-        this.id = id;
-    }
     
     public String getCompany() {
         return company;
